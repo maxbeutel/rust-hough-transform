2c4c91edf6be617c1a8046017d7c68287af1787e
Max Beutel <me@maxbeutel.de>
2016-11-11 09:26:42 +0800

    cleanup, implement edge detection based on contrast, image is still kind of off

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,15 @@ use std::path::Path;
 
 use na::{DMatrix, min};
 
-use image::{ImageLuma8, ImageBuffer, GenericImage, Pixel};
+use image::{ImageBuffer, GenericImage, Pixel};
 
-fn init(input_path: &str, output_path: &str, theta_axis_size: u32, r_axis_size: u32) {
+fn init(
+    input_path: &str,
+    output_path: &str,
+    min_contrast: u32,
+    theta_axis_size: u32,
+    r_axis_size: u32
+) {
     let img = image::open(&Path::new(input_path)).unwrap();
 
     let img_width = img.dimensions().0;
@@ -20,13 +26,9 @@ fn init(input_path: &str, output_path: &str, theta_axis_size: u32, r_axis_size: 
     let max_radius = ((img_width as f32).hypot(img_height as f32)).ceil();
     let r_axis_half = ((r_axis_size as f32) / 2.0).round();
 
-    let min_contrast = 64;
+    let mut accumulator = DMatrix::from_element(theta_axis_size as usize, r_axis_size as usize, 0);
 
-    // getArrayDataFromImage returns an array of img_width x img_height,
-    // with "average rgb value" a bit adjusted using
-    // rgbValue = (int)(((rgbValue & 0xFF0000) >>> 16) * 0.30 + ((rgbValue & 0xFF00) >>> 8) * 0.59 + (rgbValue & 0xFF) * 0.11);
-    let mut output_data = DMatrix::from_element(theta_axis_size as usize, r_axis_size as usize, 0);
-
+    // iterate over each pixel, if it's an edge, count it in the accumulator
     for (x, y, pixel) in img.pixels() {
         let r = pixel.channels()[0];
         let g = pixel.channels()[1];
@@ -34,74 +36,74 @@ fn init(input_path: &str, output_path: &str, theta_axis_size: u32, r_axis_size: 
 
         let average_rgb = (((r as f32) * 0.3 + (g as f32) * 0.59 + (b as f32) * 0.11) / 3.0).round() as u32;
 
-        // veeery simple edge detection
-        if average_rgb > min_contrast {
+        if !is_edge(&accumulator, (x, y, average_rgb), (img_width, img_height), min_contrast) {
             continue;
         }
 
-        // we found an edge!
+        // we found an edge, now go through all possible angles
         for theta in (0..theta_axis_size).rev() {
             let r = (x as f32) * ((theta as f32).to_radians()).cos() + (y as f32) * ((theta as f32).to_radians()).sin();
             let r_scaled = ((r * r_axis_half / max_radius).round() + r_axis_half) as u32;
-            // int rScaled = (int)Math.round(r * halfRAxisSize / maxRadius) + halfRAxisSize;
-            // println!("r {} of {} scaled {}", r, theta, r_scaled);
-            // let r = (x as f32) * (theta as f32).cos() + (y as f32) * (theta as f32).sin();
 
             // accumulate
-            output_data[(theta as usize, r_scaled as usize)] += 1;
+            accumulator[(theta as usize, r_scaled as usize)] += 1;
         }
-
-        //for (int theta = thetaAxisSize - 1; theta >= 0; theta--)
-
-        //println!("average: {}", average_rgb);
-
-//        if !is_edge(x, y, img_width, img_height, min_contrast, average_rgb) {
-//            continue;
-//        }
     }
 
-
     // now write output image based on accumulator
-    // @TODO writeOutputImage
-    let accu_clone = output_data.clone().into_vector();
-    let max = *accu_clone.iter().max().unwrap();// max what??
+    let accu_clone = accumulator.clone().into_vector();
+    let max = *accu_clone.iter().max().unwrap(); // max what??
 
-    let mut out = ImageBuffer::new(img_width, img_height); //ImageBuffer::new(img_width, img_height);
+    let out_img_width = accumulator.nrows() as u32;
+    let out_img_height = accumulator.ncols() as u32; // this seems kind of wrong, cols are the height???
 
-    for y in 0..img_height {
-        for x in 0..img_width {
+    let mut out = ImageBuffer::new(out_img_width, out_img_height);
 
-            let n = min(((output_data[(x as usize, y as usize)] as f32) * 255.0 / (max as f32)).round() as u32, 255) as u8;
+    for y in 0..out_img_height {
+        for x in 0..out_img_width {
+            let n = min(((accumulator[(x as usize, y as usize)] as f32) * 255.0 / (max as f32)).round() as u32, 255) as u8;
             let pixel = image::Rgb([n, n, n]);
+
             out[(x, y)] = pixel;
         }
     }
 
-    let ref mut fout = File::create(&Path::new("fractal.png")).unwrap();
+    let ref mut fout = File::create(&Path::new(output_path)).unwrap();
     let _ = image::ImageRgb8(out).save(fout, image::PNG);
 }
 
-// fn is_edge(x: u32, y: u32, img_width: u32, img_height: u32, min_contrast: u32, average_rgb: u32) -> bool {
-//     for i in (0..8).rev() {
-//         if i == 4 {
-//           continue;
-//         }
+fn is_edge(accumulator: &DMatrix<u32>, pixel_data: (u32, u32, u32), img_dimensions: (u32, u32), min_contrast: u32) -> bool {
+    let average_rgb = pixel_data.2;
 
-//         let newx = x as i32 + (i as i32 % 3) - 1;
-//         let newy = y as i32 + (i as i32/ 3) - 1;
+    // 3 x 3 matrix to check neighboring pixels
+    for i in (0..8).rev() {
+        if i == 4 {
+            continue;
+        }
 
-//         if (newx < 0) || (newx >= img_width as i32) || (newy < 0) || (newy >= img_height as i32) {
-//             continue;
-//         }
+        let new_x = pixel_data.0 as i32 + (i as i32 % 3) - 1;
+        let new_y = pixel_data.1 as i32 + (i as i32/ 3) - 1;
 
-//         if (average_rgb - center_value).abs() >= min_contrast {
-//             return true;
-//         }
-//     }
+        if (new_x < 0) || (new_x >= img_dimensions.0 as i32) || (new_y < 0) || (new_y >= img_dimensions.1 as i32) {
+            continue;
+        }
 
-//     false
-// }
+        let neighbor_average_rgb = accumulator[(new_x as usize, new_y as usize)];
+
+        if ((neighbor_average_rgb as i32) - (average_rgb as i32)).abs() >= (min_contrast as i32) {
+            return true;
+        }
+    }
+
+    false
+}
 
 fn main() {
-    init("/Users/max/Desktop/hough5.png", "/tmp/out.png", 640, 480);
+    let input_path = "/Users/max/Desktop/hough5.png";
+    let output_path = "/tmp/hough_space.png";
+    let min_contrast = 85;
+    let theta_axis_size = 640;
+    let r_axis_size = 480;
+
+    init(input_path, output_path, min_contrast, theta_axis_size, r_axis_size);
 }
