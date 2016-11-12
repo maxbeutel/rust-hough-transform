f6624960251206395a9e92aff387fbb1e95bbcd9
Max Beutel <me@maxbeutel.de>
2016-11-12 11:37:35 +0800

    fix algorithm based on reverse engineered java version

--- src/main.rs
+++ src/main.rs
@@ -23,30 +23,47 @@ fn init(
     let img_width = img.dimensions().0;
     let img_height = img.dimensions().1;
 
+    let mut greyscale_data = DMatrix::from_element(img_width as usize, img_height as usize, 0);
+
+    // Flip y axis when reading image
+    for y in 0..img_height {
+        for x in 0..img_width {
+            let pixel = img.get_pixel(x, y);
+            let r = pixel.channels()[0];
+
+            // @TODO convert to greyscale here!
+            let y_offset = img_height - 1 - y;
+            greyscale_data[(x as usize, y_offset as usize) ] = r;
+            //println!("at {} {}/{} = {}", (y * img_width + x), x, y_offset, r);
+        }
+    }
+
     let max_radius = ((img_width as f32).hypot(img_height as f32)).ceil();
     let r_axis_half = ((r_axis_size as f32) / 2.0).round();
-
     let mut accumulator = DMatrix::from_element(theta_axis_size as usize, r_axis_size as usize, 0);
 
-    // iterate over each pixel, if it's an edge, count it in the accumulator
-    for (x, y, pixel) in img.pixels() {
-        let r = pixel.channels()[0];
-        let g = pixel.channels()[1];
-        let b = pixel.channels()[2];
+    for y in 0..img_height {
+        for x in 0..img_width {
+            let i = greyscale_data[(x as usize, y as usize)];
+            //println!("color at {}/{} is {}", x, y, i);
+            if is_edge(&greyscale_data, (x, y, i as u32), (img_width, img_height), min_contrast) {
+                //println!("detected edge: {}/{}", x, y);
 
-        let average_rgb = (((r as f32) * 0.3 + (g as f32) * 0.59 + (b as f32) * 0.11) / 3.0).round() as u32;
+                // we found an edge, now go through all possible angles
+                for theta in (0..theta_axis_size).rev() {
+                    let theta_radians = theta as f64 * std::f64::consts::PI / theta_axis_size as f64;
+                    let sin = theta_radians.sin();
+                    let cos = theta_radians.cos();
 
-        if !is_edge(&accumulator, (x, y, average_rgb), (img_width, img_height), min_contrast) {
-            continue;
-        }
+                    let r = (x as f64) * cos + (y as f64) * sin;
+                    let r_scaled = ((r * r_axis_half as f64 / max_radius as f64).round() + r_axis_half as f64) as u32;
 
-        // we found an edge, now go through all possible angles
-        for theta in (0..theta_axis_size).rev() {
-            let r = (x as f32) * ((theta as f32).to_radians()).cos() + (y as f32) * ((theta as f32).to_radians()).sin();
-            let r_scaled = ((r * r_axis_half / max_radius).round() + r_axis_half) as u32;
+                    //println!("accu {}/{} at {}/{}", theta, r, x, y);
 
-            // accumulate
-            accumulator[(theta as usize, r_scaled as usize)] += 1;
+                    // accumulate
+                    accumulator[(theta as usize, r_scaled as usize)] += 1;
+                }
+            }
         }
     }
 
@@ -57,11 +74,14 @@ fn init(
     let out_img_width = accumulator.nrows() as u32;
     let out_img_height = accumulator.ncols() as u32; // this seems kind of wrong, cols are the height???
 
+    //println!("out buffered {}/{}", out_img_width, out_img_height);
+
     let mut out = ImageBuffer::new(out_img_width, out_img_height);
 
     for y in 0..out_img_height {
         for x in 0..out_img_width {
             let n = min(((accumulator[(x as usize, y as usize)] as f32) * 255.0 / (max as f32)).round() as u32, 255) as u8;
+            //println!("n {} at {}/{}", n, x, y);
             let pixel = image::Rgb([n, n, n]);
 
             out[(x, y)] = pixel;
@@ -72,25 +92,33 @@ fn init(
     let _ = image::ImageRgb8(out).save(fout, image::PNG);
 }
 
-fn is_edge(accumulator: &DMatrix<u32>, pixel_data: (u32, u32, u32), img_dimensions: (u32, u32), min_contrast: u32) -> bool {
+fn is_edge(greyscale_data: &DMatrix<u8>, pixel_data: (u32, u32, u32), img_dimensions: (u32, u32), min_contrast: u32) -> bool {
     let average_rgb = pixel_data.2;
 
+    //println!("at {}/{} - center value: {}", pixel_data.0, pixel_data.1, average_rgb);
+
     // 3 x 3 matrix to check neighboring pixels
-    for i in (0..8).rev() {
+    // @TODO can use nalgebtra matrix for this?
+    for i in (0..9).rev() {
         if i == 4 {
             continue;
         }
 
         let new_x = pixel_data.0 as i32 + (i as i32 % 3) - 1;
-        let new_y = pixel_data.1 as i32 + (i as i32/ 3) - 1;
+        let new_y = pixel_data.1 as i32 + (i as i32 / 3) - 1;
+
+        //println!("new xy {}/{}", new_x, new_y);
 
         if (new_x < 0) || (new_x >= img_dimensions.0 as i32) || (new_y < 0) || (new_y >= img_dimensions.1 as i32) {
             continue;
         }
 
-        let neighbor_average_rgb = accumulator[(new_x as usize, new_y as usize)];
+        let r = greyscale_data[(new_x as usize, new_y as usize)] as u32;
+        // @TODO ^ fix this, this is not average RGB, need to convert to greyscale!
 
-        if ((neighbor_average_rgb as i32) - (average_rgb as i32)).abs() >= (min_contrast as i32) {
+        //println!("neighbor rgb: {}", r);
+
+        if ((r as i32) - (average_rgb as i32)).abs() >= (min_contrast as i32) {
             return true;
         }
     }
@@ -100,10 +128,11 @@ fn is_edge(accumulator: &DMatrix<u32>, pixel_data: (u32, u32, u32), img_dimensio
 
 fn main() {
     let input_path = "/Users/max/Desktop/hough5.png";
-    let output_path = "/tmp/hough_space.png";
-    let min_contrast = 85;
+    let output_path = "hough_space.png";
+
     let theta_axis_size = 640;
     let r_axis_size = 480;
+    let min_contrast = 64;
 
     init(input_path, output_path, min_contrast, theta_axis_size, r_axis_size);
 }
